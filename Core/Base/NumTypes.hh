607f6d2be8325bdd60ea5d907f95cc99ef78e6a1
Paolo Jovon <paolo.jovon@gmail.com>
2018-05-24 13:28:41 +0100

    Added a NumTypes header containing numeric typedefs

---
+++ Core/Base/NumTypes.hh
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace Ares
+{
+
+using I8 = int8_t; ///< A signed 8-bit integer.
+using I16 = int16_t; ///< A signed 16-bit integer.
+using I32 = int32_t; ///< A signed 32-bit integer.
+using I64 = int64_t; ///< A signed 64-bit integer.
+
+using U8 = uint8_t; ///< An unsigned 8-bit integer.
+using U16 = uint16_t; ///< An unsigned 16-bit integer.
+using U32 = uint32_t; ///< An unsigned 32-bit integer.
+using U64 = uint64_t; ///< An unsigned 64-bit integer.
+
+using F32 = float; ///< A 32-bit floating point number.
+using F64 = double; ///< A 64-bit floating point number.
+
+}
