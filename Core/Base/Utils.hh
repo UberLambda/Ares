abb14b4c1f79c095de19715ee9ad21e7c7391b80
Paolo Jovon <paolo.jovon@gmail.com>
2018-06-06 12:48:07 +0100

    Added some Utils functions for iterators

--- Core/Base/Utils.hh
+++ Core/Base/Utils.hh
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <stddef.h>
+#include <tuple>
+
 namespace Ares
 {
 
@@ -14,9 +17,70 @@ inline T min(T a, T b)
 /// Returns the maximum between two `T`s.
 /// If `a` and `b` are equal, returns `b`.
 template <typename T>
-T max(T a, T b)
+inline T max(T a, T b)
 {
     return a > b ? a : b;
+}
+
+
+/// Returns true if any of `*it` evaluate to true, for `it` in `begin..end`.
+/// Lazy: stops iterating on the first "truthy" iterator found.
+template <typename Iter>
+inline bool any(Iter begin, Iter end)
+{
+    for(Iter it = begin; it != end; it ++)
+    {
+        if(*it)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/// Returns true if any of `func(*it)` evaluates to true, for `it` in `begin..end`.
+/// Lazy: stops iterating on the first "truthy" iterator found.
+template <typename Iter, typename Func>
+inline bool any(Iter begin, Iter end, Func func)
+{
+    for(Iter it = begin; it != end; it ++)
+    {
+        if(func(*it))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/// Returns true if all of `*it` evaluate to true, for `it` in `begin..end`.
+/// Lazy: stops iterating on the first "falsey" iterator found.
+template <typename Iter>
+inline bool all(Iter begin, Iter end)
+{
+    for(Iter it = begin; it != end; it ++)
+    {
+        if(!(*it))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+/// Returns true if all of `func(*it)` evaluate to true, for `it` in `begin..end`.
+/// Lazy: stops iterating on the first "truthy" iterator found.
+template <typename Iter, typename Func>
+inline bool all(Iter begin, Iter end, Func func)
+{
+    for(Iter it = begin; it != end; it ++)
+    {
+        if(!func(*it))
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 }
