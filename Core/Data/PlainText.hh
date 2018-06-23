03b52eca80368370bae93689b09449fecbdd9e41
Paolo Jovon <paolo.jovon@gmail.com>
2018-06-23 19:30:49 +0100

    Added a PlainText resource type

---
+++ Core/Data/PlainText.hh
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include "ResourceParser.hh"
+
+namespace Ares
+{
+
+/// A plain text resource.
+using PlainText = std::string;
+
+
+template <>
+struct ResourceParser<PlainText>
+{
+    static bool parse(PlainText& outResource, std::istream& stream, const char* ext)
+    {
+        stream.seekg(0, std::ios::end);
+        outResource.resize(stream.tellg());
+        stream.seekg(0, std::ios::beg);
+
+        stream.read(&outResource[0], outResource.length());
+
+        return true; // (parsing a plain text file never fails)
+    }
+};
+
+}
