7983f56721e987bb8e0519335ec62dc6f92bffd0
Paolo Jovon <paolo.jovon@gmail.com>
2018-06-24 12:22:25 +0100

    Started properly stubbing out the graphics module

--- Core/Gfx/GfxModule.hh
+++ Core/Gfx/GfxModule.hh
@@ -5,8 +5,24 @@
 namespace Ares
 {
 
+class Window; // (#include "../Visual/Window.hh")
+
+/// A graphics + graphical input module.
 class GfxModule : public Module
 {
+    Window* window_; // FIXME: See `GfxModule::init()` implementation
+
+    struct RenderData;
+    RenderData* renderData_;
+
+    /// Attempts to initialize `window_`, returns `false` on error.
+    // TODO Selectable rendering API for the window
+    bool initWindow(Core& core);
+
+    /// Attempts to initialize OpenGL, returns `false` on error.
+    /// `window_` should be inited for OpenGL 3.3+.
+    bool initGL(Core& core);
+
 public:
     GfxModule();
     ~GfxModule() override;
