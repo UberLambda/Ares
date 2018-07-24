8147f20cbdc78b51f4e1ed5cd55c9e78120491f0
Paolo Jovon <paolo.jovon@gmail.com>
2018-07-24 17:31:23 +0200

    Added a method to get a transform matrix out of a TransformComp and some defaults for position, rotation, scale

--- Core/Comp/TransformComp.hh
+++ Core/Comp/TransformComp.hh
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/euler_angles.hpp>
 
 namespace Ares
 {
@@ -9,15 +13,25 @@ namespace Ares
 struct TransformComp
 {
     /// The position of the entity in the scene.
-    glm::vec3 position;
+    glm::vec3 position{0.0f, 0.0f, 0.0f};
 
     /// The XYZ Euler rotation of the entity.
     /// Angles are in radians.
-    glm::vec3 rotation;
+    glm::vec3 rotation{0.0f, 0.0f, 0.0f};
 
     /// A scaling factor for the entity.
     /// (1.0, 1.0, 1.0) is the base scale.
-    glm::vec3 scale;
+    glm::vec3 scale{1.0f, 1.0f, 1.0f};
+
+
+    /// Returns a matrix representation of the transform.
+    inline glm::mat4 matrix() const
+    {
+        glm::mat4 t = glm::translate(glm::mat4(1.0f), position);
+        glm::mat4 r = glm::eulerAngleXYZ(rotation.x, rotation.y, rotation.z);
+        glm::mat4 s = glm::scale(glm::mat4(1.0f), scale);
+        return t * r * s;
+    }
 };
 
 }
